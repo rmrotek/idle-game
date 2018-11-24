f07fc066f952b7f212f11ef334cecc9118cef6b6
rmrotek <remek87@gmail.com>
2018-11-25 00:49:40 +0100

    map state/dispatch to props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 
 class App extends Component {
   totalClickPower() {
-    const {upgradeOne, upgradeTwo} = this.props.score.clickPowerUpgrades
+    const { upgradeOne, upgradeTwo } = this.props.clickPowerUpgrades
     return 1 + upgradeOne + upgradeTwo
   }
 
@@ -14,13 +14,13 @@ class App extends Component {
       <>
         <div className='score-area'>
           <p>
-            score: {this.props.score.currentScore}
+            score: {this.props.currentScore}
           </p>
           <p>
             click power: {this.totalClickPower()}
           </p>
           <p>
-            auto power: {this.props.score.autoPower}
+            auto power: {this.props.currentAutoPower}
           </p>
 
         </div>
@@ -30,16 +30,16 @@ class App extends Component {
         </div>
 
         <div className='click-area'>
-          <button onClick={() => this.props.dispatch({ type: 'INCREMENT', payload: this.totalClickPower() })}>clickable</button>
+          <button onClick={() => this.props.increment(this.totalClickPower())}>clickable</button>
         </div>
 
         <div className='upgrade-area'>
-          <button onClick={() => this.props.dispatch({ type: 'CLICK_BUY_ONE', payload: 3 })}>up to click power</button>
-          <button onClick={() => this.props.dispatch({ type: 'CLICK_BUY_TWO', payload: 4 })}>up to click power</button>
+          <button onClick={() => this.props.buyClickUpgradeOne(3)}>up to click power</button>
+          <button onClick={() => this.props.buyClickUpgradeTwo(4)}>up to click power</button>
 
         </div>
         <div className='minion-area'>
-          <button onClick={() => this.props.dispatch({ type: 'AUTO_BUY', payload: 10 })}>up to auto power</button>
+          <button onClick={() => this.props.buyAutoUpgradeOne(10)}>up to auto power</button>
         </div>
       </>
 
@@ -48,6 +48,36 @@ class App extends Component {
   }
 }
 
-export default connect(state => ({
-  score: state.score
-}))(App);
+const mapStateToProps = state => ({
+  currentScore: state.score.currentScore,
+  currentAutoPower: state.score.autoPower,
+  clickPowerUpgrades: state.score.clickPowerUpgrades
+});
+
+const mapDispatchToProps = dispatch => ({
+  increment: (power) =>
+    dispatch({
+      type: "INCREMENT",
+      payload: power
+    }),
+  buyClickUpgradeOne: power =>
+    dispatch({
+      type: "CLICK_BUY_ONE",
+      payload: power
+    }),
+  buyClickUpgradeTwo: power =>
+    dispatch({
+      type: "CLICK_BUY_TWO",
+      payload: power
+    }),
+  buyAutoUpgradeOne: power =>
+    dispatch({
+      type: "AUTO_BUY",
+      payload: power
+    })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
